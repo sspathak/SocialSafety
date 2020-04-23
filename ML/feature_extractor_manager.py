a8a14510f62a091e625d0b4a2044f1fb537dffcd
sspathak <suraj.s.pathak@gmail.com>
2020-04-23 15:42:49 -0400

    feature_extractor_manager complete needs testing with real data

---
+++ ML/feature_extractor_manager.py
@@ -0,0 +1,23 @@
+from .feature_extractors import MFCC
+from .feature_extractors import power_spectral_density
+from .feature_extractors import spectral_centroid
+from .feature_extractors import spectral_flux
+from .feature_extractors import spectral_rolloff
+import numpy as np
+
+feature_funciton_array = [
+    MFCC.get_feature_vector,
+    power_spectral_density,
+    spectral_centroid,
+    spectral_flux,
+    spectral_rolloff,
+]
+
+# data = csv line
+def get_combined_feature_vector(data):
+    grand_feature_vector = (np.array([i(data) for i in feature_funciton_array])).flatten
+    return grand_feature_vector
+
+if __name__ == "__main__":
+    data = [None]
+    get_combined_feature_vector(data)
