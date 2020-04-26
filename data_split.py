21c056f2f24c8805a7f1018b716412b013ff25e7
sspathak <suraj.s.pathak@gmail.com>
2020-04-26 17:27:04 -0400

    Split CSVs to reduce size

--- data_split.py
+++ data_split.py
@@ -1,15 +1,16 @@
-file_path = "cough-data-cough-0.csv"
+file_path = "./CSVs/cough-data-throatclearing-1.csv"
 
-save_path = "./CSVs/cough-data-cough-1.csv"
+save_path = "./CSVs/cough-data-throatclearing-3.csv"
 
 with open(file_path,'r') as fil:
-    fil.readlines(500)
+    for j in range(200 * 1):
+        fil.readline()
     # input(len(fil.readlines()))
     # dat = fil.readline()
     with open(save_path,'w') as wfil:
-        for i in range(1000):
-            if i % 50 == 0:
-                fil.readlines(50)
-                print(i)
+        for i in range(200):
+            # if i % 50 == 0:
+            #     fil.readlines(50)
+            #     print(i)
             wfil.writelines(fil.readline())
 print("done")
