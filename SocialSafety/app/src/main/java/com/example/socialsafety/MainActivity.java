01c28f6d10efd75fbdb49d068513f09d569b021b
ayushkhd <ayushkhd@gmail.com>
2020-04-29 17:36:04 +0530

    fixed BT bugs

--- SocialSafety/app/src/main/java/com/example/socialsafety/MainActivity.java
+++ SocialSafety/app/src/main/java/com/example/socialsafety/MainActivity.java
@@ -1,69 +1,58 @@
 package com.example.socialsafety;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     BluetoothAdapter mBluetoothAdapter;
     public ArrayList<BluetoothDevice> list;
-    StringBuilder sbName = new StringBuilder();
-    StringBuilder sbAdd = new StringBuilder();
 
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-                Log.d("XXX", deviceName);
-            }
-        }
-    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button toggleBtn = (Button) findViewById(R.id.buttonToggle);
-        Button scan = (Button) findViewById((R.id.scan));
+        ToggleButton scan = (ToggleButton) findViewById((R.id.scanBtn));
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         list = new ArrayList<BluetoothDevice>();
 
-        scan.setOnClickListener(new View.OnClickListener() {
+        scan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Log.d("x","Pressed");
-                if(mBluetoothAdapter.disable())
-                    enableDisableBT();
-                if(mBluetoothAdapter.isDiscovering()){
-                    mBluetoothAdapter.cancelDiscovery();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                checkPerm();
+                Log.d("Mani", "ClickedToggle");
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                if(isChecked){
+                    registerReceiver(bReciever, filter);
                     mBluetoothAdapter.startDiscovery();
-                    IntentFilter scanner = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-                    registerReceiver(receiver, scanner);
-                    Log.d("s", "isDiscovenig");
                 }
-                if(!mBluetoothAdapter.isDiscovering()){
-                    IntentFilter scanner = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-                    registerReceiver(receiver, scanner);
-                    Log.d("Tr", "Trying to on");
+                else{
+                    unregisterReceiver(bReciever);
+                    mBluetoothAdapter.cancelDiscovery();
                 }
             }
         });
+
+
+
 
         toggleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +63,22 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
+    private final BroadcastReceiver bReciever = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Create a new device item
+//                DeviceItem newDevice = new DeviceItem(device.getName(), device.getAddress(), "false");
+                // Add it to our adapter
+//                mAdapter.add(newDevice);
+                Log.d("FFF", "FOUND SOMETHING"+ device.getName());
+            }
+        }
+    };
+
     public void enableDisableBT(){
         if(mBluetoothAdapter == null){
             Log.d("Not working", "BT not supported");
@@ -86,6 +91,13 @@ public class MainActivity extends AppCompatActivity {
         if(mBluetoothAdapter.isEnabled()){
             mBluetoothAdapter.disable();
         }
+    }
+
+     public void checkPerm(){
+        int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
     }
 
 }
